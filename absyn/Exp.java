a943c2f9dba888f0c0dfb39db3535382fd54d929
Neivin Mathew <neivin93@gmail.com>
2017-03-03 20:20:09 -0500

    Make Exp abstract

--- absyn/Exp.java
+++ absyn/Exp.java
@@ -1,24 +1,4 @@
 package absyn;
 
-public class Exp extends Absyn {
-	public Var identifier;
-	public Exp expr;
-	public SimpleExp simpleExp;
-
-	public Exp (int pos, SimpleExp simpleExp){
-		this.pos = pos;
-		this.simpleExp = simpleExp;
-
-		this.expr = null;
-		this identifier = null;
-	}
-
-	public Exp (int pos, Var identifier, Exp expr){
-		this.pos = pos;
-		this.identifier = identifier;
-		this.expr = expr;
-		
-		this.simpleExp = null;
-	}
-
+public abstract class Exp extends Absyn {
 }
