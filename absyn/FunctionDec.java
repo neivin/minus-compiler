42a263907cc9ee2f21066f64eeeae155ae3034b0
Neivin Mathew <neivin93@gmail.com>
2017-03-04 19:30:33 -0500

    Fix type mismatches; Add missing VarDecList production

--- absyn/FunctionDec.java
+++ absyn/FunctionDec.java
@@ -4,9 +4,9 @@ public class FunctionDec extends Dec {
 	public Type result;
 	public String func;
 	public VarDecList params;
-	public CompoundExp body;
+	public Exp body;
 
-	public FunctionDec (int pos, Type result, String func, VarDecList params, CompoundExp body){
+	public FunctionDec (int pos, Type result, String func, VarDecList params, Exp body){
 		this.pos = pos;
 		this.result = result;
 		this.func = func;
