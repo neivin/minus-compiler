0363e6e5fa9025ba9bc83c51e79fee0e82b4719d
Neivin Mathew <neivin93@gmail.com>
2017-03-05 16:53:56 -0500

    Add command line option -a

--- Main.java
+++ Main.java
@@ -1,9 +1,31 @@
 import java.io.*;
+import java.util.*;
 
 class Main{
-  static public void main(String argv[]){
+  public static void main(String [] argv){
+    
+    public boolean astMode;
+    public String file;
+
+    if (argv.length > 2 ){
+    	System.err.println("Error: Invalid number of arguments");
+    	System.err.println("The correct usage is:");
+    	System.err.println("\tjava Main <c-minus-file>.cm");
+    	System.exit(0);
+    }
+
+    for (String arg : argv){
+    	if (arg.equals("-a")){
+    		astMode = true;
+    	}
+    	else{
+    		file = arg;
+    	}
+    }
+
     try {
-      parser p = new parser(new Lexer(new FileReader(argv[0])));
+      parser p = new parser(new Lexer(new FileReader(file)));
+      p.astMode = astMode;
       Object result = p.parse().value;
     } catch (Exception e) {
       /* do cleanup here -- possibly rethrow e */
